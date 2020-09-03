e2377fe75645da28ec066c8f0a6f74b72e31876e
Anna <atharayil@hawk.iit.edu>
2020-09-03 16:23:20 -0700

    Adding thought process to some files

---
+++ Java/src/ConvertIntegerBinary.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class ConvertIntegerBinary {
+
+    public static String convert_to_binary(int decimal, String output) {
+        if (decimal <= 1) {
+            output = String.valueOf(decimal) + output;
+            return output;
+        } else
+        {
+            output = String.valueOf(decimal % 2) + output;
+            decimal = decimal / 2;
+            return convert_to_binary(decimal, output);
+        }
+
+    }
+
+    public static void main(String args[]){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //String s = br.readLine();
+        int input_decimal = 4;
+        String output  = "";
+        System.out.println(input_decimal);
+        String ans = convert_to_binary(input_decimal, output);
+        System.out.println(ans);
+
+    }
+}
