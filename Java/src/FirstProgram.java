d69076430bda2eb4ce2885f61f249304b5d3d2cd
Anna <atharayil@hawk.iit.edu>
2020-09-09 15:22:07 -0700

    Adding new programs

--- Java/src/FirstProgram.java
+++ Java/src/FirstProgram.java
@@ -1,6 +1,16 @@
 public class FirstProgram {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+        int x = 10;
+        while(x <10){
+            System.out.println(x);
+            x++;
+        }
+        int y = 19;
+        do{
+            System.out.println("DO"+y);
+            y++;
+        } while(y <= 20);
+
     }
 }
